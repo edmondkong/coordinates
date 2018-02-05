888e7559a3353b3b48d0c654faa1c01e93487e37
Edmond Kong <edmond.k94@gmail.com>
2018-02-05 17:18:20 +0000

    Generate points until a point within the polygon is found

--- coordinates.py
+++ coordinates.py
@@ -23,6 +23,30 @@ def get_country_polygon(data, country):
     return max(next(x for x in data if x['id'] == country)['geometry']['coordinates'], key=len)
 
 
+def inside_polygon(x, y, points):
+    """
+    Return True if a coordinate (x, y) is inside a polygon defined by
+    a list of verticies [(x1, y1), (x2, x2), ... , (xN, yN)].
+
+    Reference: http://www.ariel.com.au/a/python-point-int-poly.html
+    """
+    points = points[0]
+    n = len(points)
+    inside = False
+    p1x, p1y = points[0]
+    for i in range(1, n + 1):
+        p2x, p2y = points[i % n]
+        if y > min(p1y, p2y):
+            if y <= max(p1y, p2y):
+                if x <= max(p1x, p2x):
+                    if p1y != p2y:
+                        xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                    if p1x == p2x or x <= xinters:
+                        inside = not inside
+        p1x, p1y = p2x, p2y
+    return inside
+
+
 def get_random_point_within_polygon(polygon):
     x_points = map(lambda bound: bound[0], polygon[0])
     y_points = map(lambda bound: bound[1], polygon[0])
@@ -43,4 +67,15 @@ def get_random_point_within_country(data, country):
 
     country_polygon = get_country_polygon(data, country)
 
-    return get_random_point_within_polygon(country_polygon)
+    point = tuple
+    valid_point = False
+
+    while not valid_point:
+        point = get_random_point_within_polygon(country_polygon)
+
+        if inside_polygon(point[0], point[1], country_polygon):
+            valid_point = True
+
+    return point
+
+print(get_random_point_within_country(load_countries_data(), 'CHN'))
